61fe41c69a236c6a2c77925ae65123e635ee92eb
otsuka <c0a2526641@edu.teu.ac.jp>
2026-04-14 16:18:44 +0900

    練習１０：こうかとんが動いた

--- pygame_image.py
+++ pygame_image.py
@@ -14,16 +14,27 @@ def main():
     kk_img = pg.image.load("fig/3.png") #練習3
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
+    kk_rct = kk_img.get_rect() #練習10：こうかとんrectを取得
+    kk_rct.center = 300, 200 #練習10：こうかとんの初期座標の設定
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        key_lst = pg.key.get_pressed() #練習10：すべてのキーの押下状態の取得
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, 1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1, 0))
         x = tmr%3200 #練習5
         screen.blit(bg_img, [-x, 0]) #練習2
         screen.blit(bg_img2, [-x+1600, 0]) #練習7
         screen.blit(bg_img, [-x+3200, 0]) #練習9
-        screen.blit(kk_img, [300, 200]) #練習4
+        screen.blit(kk_img, kk_rct) #練習4
         pg.display.update()
         tmr += 1        
         clock.tick(200) #練習6
