2e280e3ae0a662e6750bce3ac17221197da4e888
otsuka <c0a2526641@edu.teu.ac.jp>
2026-04-14 15:48:08 +0900

    練習５と６：背景画像が右から左に流れる

--- pygame_image.py
+++ pygame_image.py
@@ -10,7 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") # 練習1
-    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.image.load("fig/3.png") #練習3
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
     tmr = 0
@@ -18,11 +18,12 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0]) #練習2
-        screen.blit(kk_img, [300, 200])
+        x = tmr #練習5
+        screen.blit(bg_img, [-tmr, 0]) #練習2
+        screen.blit(kk_img, [300, 200]) #練習4
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200) #練習6
 
 
 if __name__ == "__main__":
